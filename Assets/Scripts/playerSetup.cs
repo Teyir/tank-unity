0e46498656fbf5dbb3ed2db6106b89b29a3d97c8
Teyir <60007549+Teyir@users.noreply.github.com>
2022-03-28 22:41:18 +0200

    [INIT] Start tank multi player with mirror
    Some sync buggs (shooting & animation)

---
+++ Assets/Scripts/playerSetup.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Mirror;
+
+public class playerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        
+        if (!isLocalPlayer)
+        {
+            // On va boucler sur les différents composants renseignés et les désactiver si ce joueur n'est pas le notre
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera?.gameObject.SetActive(true);
+        }
+    }
+
+}
